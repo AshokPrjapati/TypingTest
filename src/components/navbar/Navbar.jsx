d72b0c7c1534198011deccb09061a9ea32115b6f
Ashok Kumar <ap271449@gmail.com>
2023-06-07 19:59:45 +0530

    typing portion completed

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,25 +1,29 @@
-import { Link } from "react-router-dom"
-import styles from "../../styles/Navbar.module.css"
+import { Link } from "react-router-dom";
+
+import Button from "../Button";
+import styles from "../../styles/Navbar.module.css";
 
 const Navbar = () => {
-    return (
-        <header className={styles.navbar}>
-            <Link to="/" className={styles.title}>
-                <div>
-                    <img src="/images/logo.png" alt="logo" />
-                </div>
-                <h1>Typing Test</h1>
+  return (
+    <header className={styles.navbar}>
+      <Link to="/" className={styles.title}>
+        <div>
+          <img src="/images/logo.png" alt="logo" />
+        </div>
+        <h1>Typing Test</h1>
+      </Link>
+
+      <nav>
+        <ul>
+          <li>
+            <Link to="/signin">
+              <Button label="Login" small />
             </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
-            <nav>
-                <ul>
-                    <li>
-                        <Link to="/signin">Login</Link>
-                    </li>
-                </ul>
-            </nav>
-        </header>
-    )
-}
-
-export default Navbar
+export default Navbar;
--- src/components/typing/InputBox.jsx
+++ src/components/typing/InputBox.jsx
@@ -1,20 +1,19 @@
+import { useCallback, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import styles from "../../styles/Typing.module.css";
+
 import {
   setCurrentKey,
   updateKeyCount,
   updateUserInput,
 } from "../../redux/typing/typing.action";
 import { getRandomKey } from "../../helper/getRandomKeys";
-import { useEffect, useState } from "react";
 
 const InputBox = ({ inputRef }) => {
-  const { referenceKeys, isTyping, accuracy } = useSelector(
-    (store) => store.typing
-  );
+  const { referenceKeys, isTyping } = useSelector((store) => store.typing);
   const dispatch = useDispatch();
 
-  const keys = ["a", "s", "d", "e", "f", "g", "h", "j", "k", "l", ";", '"'];
+  const keys = ["a", "s", "d", "f", "g", "h", "j", "k", "l", ";", "'"];
 
   const handleChange = (e) => {
     // set random key as current key
@@ -23,20 +22,30 @@ const InputBox = ({ inputRef }) => {
 
     const inputValue = inputRef.current.value;
 
+    // value typed by user
+    let currentValue = "";
+
     // show only currently typed value in input field
     if (inputValue.length > 1) {
-      const updatedValue = inputValue.charAt(1);
-      inputRef.current.value = updatedValue;
-
-      // set typedKeys array - array contains all keys typed by user
-      dispatch(updateUserInput(updatedValue));
-      dispatch(
-        updateKeyCount(
-          randomKey,
-          referenceKeys[referenceKeys.length - 1] === updatedValue
-        )
-      );
+      currentValue = inputValue.charAt(1);
+      inputRef.current.value = currentValue;
+    } else {
+      currentValue = inputRef.current.value;
+      inputRef.current.value = currentValue;
     }
+
+    // set typedKeys array - array contains all keys typed by user
+    dispatch(updateUserInput(currentValue));
+
+    // check currentvalue and random value and return boolean
+    let isMatched;
+
+    if (referenceKeys.length === 0) isMatched = true;
+    else isMatched = referenceKeys[referenceKeys.length - 1] === currentValue;
+
+    console.log(randomKey, currentValue);
+    // update state
+    dispatch(updateKeyCount(randomKey, isMatched));
   };
 
   useEffect(() => {

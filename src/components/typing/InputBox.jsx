ae16041581f47d423b0f18d1b1300049b12a61e0
Ashok Kumar <ap271449@gmail.com>
2023-06-07 15:05:56 +0530

    added : css

--- src/components/typing/InputBox.jsx
+++ src/components/typing/InputBox.jsx
@@ -3,7 +3,7 @@ import styles from "../../styles/Typing.module.css";
 const InputBox = ({ inputRef }) => {
   return (
     <div className={styles.input_box}>
-      <input type="text" ref={inputRef} />
+      <input type="text" ref={inputRef} placeholder="Type..." />
     </div>
   );
 };
--- src/components/typing/KeyContainer.jsx
+++ src/components/typing/KeyContainer.jsx
@@ -1,6 +1,14 @@
+import { useSelector } from "react-redux";
 import styles from "../../styles/Typing.module.css";
 
 const KeyContainer = () => {
+  const isTyping = useSelector((store) => store.typing.isTyping);
+  if (!isTyping)
+    return (
+      <div className={styles.key_message}>
+        Click <span>Start Test</span> to start practice
+      </div>
+    );
   return <div className={styles.key_container}>A</div>;
 };
 
